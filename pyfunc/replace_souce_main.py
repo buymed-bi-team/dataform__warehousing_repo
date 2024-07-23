cbd4cb78bf57658f23d9137555093aaf9d2682db
minhluu99 <75202089+minhluu99@users.noreply.github.com>
2024-07-23 15:05:04 +0700

    Add source js for marts

--- pyfunc/replace_souce_main.py
+++ pyfunc/replace_souce_main.py
@@ -1,7 +1,7 @@
 from lab.replace_source import replace_source
 import os
 
-dir_path = 'C:\\Users\\bi.minh.luu\\Documents\\RAW_NOTE\\warehousing_repo\\definitions\\models\\intermediate'
+dir_path = 'C:\\Users\\bi.minh.luu\\Documents\\RAW_NOTE\\warehousing_repo\\definitions\\models\\marts'
 
 for (root,dirs,files) in os.walk(dir_path, topdown=True):
     for fname in files:
