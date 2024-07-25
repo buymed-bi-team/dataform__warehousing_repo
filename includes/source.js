38e1f76e3419afb8361b598e85911d38a82cb3ab
Minh Luu <minh.luugia@buymed.com>
2024-07-25 11:18:47 +0000

    Add location_code to dim_customer

--- includes/source.js
+++ includes/source.js
@@ -1,4 +1,5 @@
 const silver = "lakehouse-prod-394907.silver_buymed_vn";
 const gold = "lakehouse-prod-394907.gold_buymed_vn"
+const ref = "lakehouse-prod-394907.gold_buymed_vn2"
 
-module.exports = {silver,gold}
+module.exports = {silver,gold,ref}
