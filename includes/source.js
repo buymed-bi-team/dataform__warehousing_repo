cd33f8acec6ee30e71f99748a98544ffbdff342b
Minh Luu <minh.luugia@buymed.com>
2024-08-06 03:42:07 +0000

    Add platinum to source js

--- includes/source.js
+++ includes/source.js
@@ -1,5 +1,6 @@
 const silver = "lakehouse-prod-394907.silver_buymed_vn";
 const gold = "lakehouse-prod-394907.gold_buymed_vn"
 const ref = "lakehouse-prod-394907.gold_buymed_vn2"
+const platinum = "lakehouse-prod-394907.platinum_buymed_vn"
 
-module.exports = {silver,gold,ref}
+module.exports = {silver,gold,ref, platinum}
