92cb4faa663b5de195bd48a1d2ba28f31c87167b
Tu Dang <tu.dang@buymed.com>
2025-01-06 03:08:56 +0000

    add source ref

--- includes/source.js
+++ includes/source.js
@@ -5,5 +5,6 @@ const ref = "lakehouse-prod-394907.gold_buymed_vn2"
 const platinum = "lakehouse-prod-394907.platinum_buymed_vn"
 const collector = "lakehouse-prod-394907.collector"
 const circa = "lakehouse-prod-394907.buymed_circa"
+const circa_os = "lakehouse-prod-394907.buymed_circa_v2"
 
-module.exports = {silver,gold,ref, platinum, silver_restricted, collector,circa}
+module.exports = {silver,gold,ref, platinum, silver_restricted, collector,circa, circa_os}
