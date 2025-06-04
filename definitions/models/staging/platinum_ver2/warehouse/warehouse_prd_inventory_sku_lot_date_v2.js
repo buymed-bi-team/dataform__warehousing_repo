e996f4af5764979fa317954ad6a7b33604425261
Minh Luu <minh.luugia@buymed.com>
2025-06-04 04:56:46 +0000

    Deploy warehouse_prd_inventory_sku_lot_date_v2_platinum

--- definitions/models/staging/platinum_ver2/warehouse/warehouse_prd_inventory_sku_lot_date_v2.js
+++ definitions/models/staging/platinum_ver2/warehouse/warehouse_prd_inventory_sku_lot_date_v2.js
@@ -2,7 +2,7 @@
 const model = new main.PlatinumModel(
     "warehouse_prd_inventory_sku_lot_date_v2",
     {
-        // target_schema : "dataform_playground", // Use for document
+        target_schema : "platinum_buymed_vn__stg", // Use for document
         // customAssertionsSchema : `dataform_playground_assertions`,
         // source_schema = source.silver,
         // description = ``,
@@ -15,14 +15,14 @@ const model = new main.PlatinumModel(
     }
 );
 model.createIncremental(
-    ["platinum_custom_1h_past7", "warehouse_prd_inventory"],
+    ["0h00", "warehouse_prd_inventory"],
     {
         has_src_created_date : false,
         ingestCutOffInterval : "INTERVAL 1 MONTH"
     }
 );
 model.assertUnique(
-    ["platinum_custom_1h_past7", "warehouse_prd_inventory"],
+    ["0h00", "warehouse_prd_inventory"],
     {
         intervalCheckpoint : `INTERVAL 1 YEAR`,
         maxRetry: 2
@@ -37,7 +37,7 @@ model.createMismatchAssertionView(
     }
 )
 model.assertMismatch(
-    ["platinum_custom_1h_past7", "warehouse_prd_inventory"],
+    ["0h00", "warehouse_prd_inventory"],
     {
         intervalCheckpoint: `INTERVAL 2 DAY`,
         maxRetry: 2,
