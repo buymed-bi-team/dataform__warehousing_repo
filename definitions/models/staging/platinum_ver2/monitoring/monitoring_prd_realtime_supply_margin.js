75bd1f75aac4aac73ded2f9ac85aebb781ee24a2
Minh Luu <minh.luugia@buymed.com>
2025-09-11 10:21:18 +0000

    [Platinum] Upgrade monitoring_prd_realtime_supply_margin

---
+++ definitions/models/staging/platinum_ver2/monitoring/monitoring_prd_realtime_supply_margin.js
@@ -0,0 +1,47 @@
+
+const model = new main.PlatinumModel(
+    "monitoring_prd_realtime_supply_margin",
+    {
+        target_schema : "platinum_buymed_vn__stg", // Use for document
+        // customAssertionsSchema : `dataform_playground_assertions`,
+        // source_schema = source.silver,
+        // description = ``,
+
+        has_lastUpdatedTime : false,
+        has_createdTime : true,
+        bigquery: {
+            partitionBy: "created_date",
+            // clusterBy: [""]
+        }
+    }
+);
+model.createIncremental(
+    ["0h00","12h00", "platinum_staging", "monitoring_prd_realtime-supply"],
+    {
+        has_src_created_date : false,
+        ingestCutOffInterval : "INTERVAL 1 MONTH"
+    }
+);
+model.assertUnique(
+    ["0h00","12h00", "platinum_unique_assertion", "monitoring_prd_realtime-supply"],
+    {
+        intervalCheckpoint : `INTERVAL 1 YEAR`,
+        maxRetry: 2
+    }
+)
+model.createMismatchAssertionView(
+    ["platinum_staging_assertion_view", "monitoring_prd_realtime-supply"],
+    {
+        intervalCheckpoint: `INTERVAL 2 DAY`,
+        maxRetry: 2,
+        // dependencies : [model.dependencies.assertUnique]
+    }
+)
+model.assertMismatch(
+    ["0h00","12h00", "platinum_staging_assertion", "monitoring_prd_realtime-supply"],
+    {
+        intervalCheckpoint: `INTERVAL 2 DAY`,
+        maxRetry: 2,
+        
+    }
+)
