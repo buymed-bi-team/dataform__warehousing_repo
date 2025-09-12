bfc21ed6e814d9bb522d7619eaffce660f8e1627
Minh Luu <minh.luugia@buymed.com>
2025-09-12 04:02:09 +0000

    [Platinum] Upgrade marketplace_prd_order_v2_invoice

---
+++ definitions/models/staging/platinum_ver2/mkp_order/marketplace_prd_order_v2_invoice.js
@@ -0,0 +1,47 @@
+
+const model = new main.PlatinumModel(
+    "marketplace_prd_order_v2_invoice",
+    {
+        target_schema : "platinum_buymed_vn__stg", // Use for document
+        // customAssertionsSchema : `dataform_playground_assertions`,
+        // source_schema = source.silver,
+        // description = ``,
+
+        has_lastUpdatedTime : true,
+        has_createdTime : true,
+        bigquery: {
+            partitionBy: "created_date",
+            clusterBy: ["delivered_time","invoice_deadline","invoice_status"]
+        }
+    }
+);
+model.createIncremental(
+    ["0h00","12h00", "platinum_staging", "marketplace_prd_order-v2"],
+    {
+        has_src_created_date : false,
+        ingestCutOffInterval : "INTERVAL 1 MONTH"
+    }
+);
+model.assertUnique(
+    ["0h00","12h00", "platinum_unique_assertion", "marketplace_prd_order-v2"],
+    {
+        intervalCheckpoint : `INTERVAL 1 YEAR`,
+        maxRetry: 2
+    }
+)
+model.createMismatchAssertionView(
+    ["platinum_staging_assertion_view", "marketplace_prd_order-v2"],
+    {
+        intervalCheckpoint: `INTERVAL 2 DAY`,
+        maxRetry: 2,
+        // dependencies : [model.dependencies.assertUnique]
+    }
+)
+model.assertMismatch(
+    ["0h00","12h00", "platinum_staging_assertion", "marketplace_prd_order-v2"],
+    {
+        intervalCheckpoint: `INTERVAL 2 DAY`,
+        maxRetry: 2,
+        
+    }
+)
