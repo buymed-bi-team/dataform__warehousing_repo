78898183f9a1535af66b10109679c22ffad66b5c
Minh Luu <minh.luugia@buymed.com>
2025-10-14 09:50:40 +0000

    [Platinum] Upgrade seller_prd_purchasing_purchase_order

---
+++ definitions/models/staging/platinum_ver2/seller/seller_prd_purchasing_purchase_order.js
@@ -0,0 +1,47 @@
+const model = new main.PlatinumModel(
+    "seller_prd_purchasing_purchase_order",
+    {
+        target_schema : "platinum_buymed_vn__stg", // Use for document
+        // customAssertionsSchema : `dataform_playground_assertions`,
+        // source_schema = source.silver,
+        // description = ``,
+
+        has_lastUpdatedTime : true,
+        has_createdTime : true,
+        bigquery: {
+            partitionBy: "created_date",
+            clusterBy: ["status","po_code"]
+        }
+    }
+);
+model.createIncremental(
+    ["platinum_0h00","platinum_12h00", "platinum_staging", "seller_prd_purchasing", "Finance/margin"],
+    {
+        has_src_created_date : false,
+        ingestCutOffInterval : "INTERVAL 1 MONTH"
+    }
+);
+model.assertUnique(
+    ["platinum_0h00","platinum_12h00", "platinum_unique_assertion", "seller_prd_purchasing"],
+    {
+        intervalCheckpoint : `INTERVAL 1 YEAR`,
+        maxRetry: 2,
+        dependencies: [{schema:"platinum_buymed_vn",name:"seller_prd_purchasing_purchase_order"}]
+    }
+)
+model.createMismatchAssertionView(
+    ["platinum_staging_assertion_view", "seller_prd_purchasing"],
+    {
+        intervalCheckpoint: `INTERVAL 2 DAY`,
+        maxRetry: 2,
+        // dependencies : [model.dependencies.assertUnique]
+    }
+)
+model.assertMismatch(
+    ["platinum_0h00","platinum_12h00", "platinum_staging_assertion", "seller_prd_purchasing"],
+    {
+        intervalCheckpoint: `INTERVAL 2 DAY`,
+        maxRetry: 2,
+        
+    }
+)
---
+++ definitions/models/staging/platinum_ver2/seller/seller_prd_purchasing_purchase_order_item.js
@@ -0,0 +1,47 @@
+const model = new main.PlatinumModel(
+    "seller_prd_purchasing_purchase_order_item",
+    {
+        target_schema : "platinum_buymed_vn__stg", // Use for document
+        // customAssertionsSchema : `dataform_playground_assertions`,
+        // source_schema = source.silver,
+        // description = ``,
+
+        has_lastUpdatedTime : true,
+        has_createdTime : true,
+        bigquery: {
+            partitionBy: "created_date",
+            clusterBy: ["product_id","warehouse_code","vendor_code","po_code"]
+        }
+    }
+);
+model.createIncremental(
+    ["platinum_0h00","platinum_12h00", "platinum_staging", "seller_prd_purchasing", "Finance/margin"],
+    {
+        has_src_created_date : false,
+        ingestCutOffInterval : "INTERVAL 1 MONTH"
+    }
+);
+model.assertUnique(
+    ["platinum_0h00","platinum_12h00", "platinum_unique_assertion", "seller_prd_purchasing"],
+    {
+        intervalCheckpoint : `INTERVAL 1 YEAR`,
+        maxRetry: 2,
+        dependencies: [{schema:"platinum_buymed_vn",name:"seller_prd_purchasing_purchase_order_item"}]
+    }
+)
+model.createMismatchAssertionView(
+    ["platinum_staging_assertion_view", "seller_prd_purchasing"],
+    {
+        intervalCheckpoint: `INTERVAL 2 DAY`,
+        maxRetry: 2,
+        // dependencies : [model.dependencies.assertUnique]
+    }
+)
+model.assertMismatch(
+    ["platinum_0h00","platinum_12h00", "platinum_staging_assertion", "seller_prd_purchasing"],
+    {
+        intervalCheckpoint: `INTERVAL 2 DAY`,
+        maxRetry: 2,
+        
+    }
+)
