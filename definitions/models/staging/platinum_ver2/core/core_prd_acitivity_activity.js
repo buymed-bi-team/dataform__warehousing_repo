cf2d13195acf3ea05284ba05b523968797658000
Minh Luu <minh.luugia@buymed.com>
2025-12-08 03:19:13 +0000

    [Platinum] Upgrade core_prd_acitivity_activity

---
+++ definitions/models/staging/platinum_ver2/core/core_prd_acitivity_activity.js
@@ -0,0 +1,48 @@
+
+
+const model = new main.PlatinumModel(
+    "core_prd_activity_activity",
+    {
+        target_schema : "platinum_buymed_vn__stg", 
+        // customAssertionsSchema : `dataform_playground_assertions`,
+        // source_schema = source.silver,
+        // description = ``,
+
+        has_lastUpdatedTime : false,
+        has_createdTime : true,
+        bigquery: {
+            partitionBy: "created_date",
+            clusterBy: ["template_code"]
+        }
+    }
+);
+model.createIncremental(
+    ["platinum_custom_30m","core_prd_activity"],
+    {
+        has_src_created_date : false,
+        ingestCutOffInterval : "INTERVAL 1 MONTH"
+    }
+);
+model.assertUnique(
+    ["platinum_09h00", "core_prd_activity"],
+    {
+        intervalCheckpoint : `INTERVAL 1 YEAR`,
+        maxRetry: 2
+    }
+)
+model.createMismatchAssertionView(
+    ["core_prd_activity"],
+    {
+        intervalCheckpoint: `INTERVAL 2 DAY`,
+        maxRetry: 2,
+        dependencies : [{"schema":"platinum_buymed_vn","name":"core_prd_activity_activity"}]
+    }
+)
+model.assertMismatch(
+    ["platinum_09h00", "core_prd_activity"],
+    {
+        intervalCheckpoint: `INTERVAL 2 DAY`,
+        maxRetry: 2,
+        
+    }
+)
