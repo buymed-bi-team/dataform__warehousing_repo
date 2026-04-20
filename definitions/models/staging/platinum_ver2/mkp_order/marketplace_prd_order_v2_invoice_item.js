820281a1372fe4ba79d750b1686af384ce747b9f
Nguyen Long <long.nguyen@buymed.com>
2026-04-20 10:29:50 +0000

    add marketplace_prd_order_v2_invoice_item .js and .sqlx for silver -> platinum

---
+++ definitions/models/staging/platinum_ver2/mkp_order/marketplace_prd_order_v2_invoice_item.js
@@ -0,0 +1,47 @@
+
+const model = new main.PlatinumModel(
+    "marketplace_prd_order_v2_invoice_item",
+    {
+        target_schema : "platinum_buymed_vn__stg",
+        has_lastUpdatedTime : true,
+        has_createdTime : true,
+        bigquery: {
+            partitionBy: "created_date"
+        }
+    }
+);
+
+model.createIncremental(
+    ["platinum_0h00","platinum_12h00", "platinum_staging", "circa-online_prd_consumer"],
+    {
+        has_src_created_date : false,
+        ingestCutOffInterval : "INTERVAL 1 MONTH"
+    }
+);
+
+model.assertUnique(
+    ["platinum_0h00","platinum_12h00", "platinum_unique_assertion", "circa-online_prd_consumer"],
+    {
+        intervalCheckpoint : `INTERVAL 1 YEAR`,
+        maxRetry: 2
+    }
+);
+
+model.createMismatchAssertionView(
+    ["platinum_staging_assertion_view", "circa-online_prd_consumer"],
+    {
+        intervalCheckpoint: `INTERVAL 2 DAY`,
+        maxRetry: 2,
+        dependencies : [
+            {schema:"platinum_buymed_vn", name:"marketplace_prd_order_v2_invoice_item"}
+        ]
+    }
+);
+
+model.assertMismatch(
+    ["platinum_0h00","platinum_12h00", "platinum_staging_assertion", "circa-online_prd_consumer"],
+    {
+        intervalCheckpoint: `INTERVAL 2 DAY`,
+        maxRetry: 2
+    }
+);
