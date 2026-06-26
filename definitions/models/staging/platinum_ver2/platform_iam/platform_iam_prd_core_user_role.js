b0b5ea04d4c4b73c8facb7180c3224d94e3ae761
Nguyen Tran <nguyen.tran@buymed.com>
2026-06-26 09:41:38 +0000

    [Platinum] Create platform_iam_prd_core_user_role

---
+++ definitions/models/staging/platinum_ver2/platform_iam/platform_iam_prd_core_user_role.js
@@ -0,0 +1,48 @@
+
+
+const model = new main.PlatinumModel(
+    "platform_iam_prd_core_user_role",
+    {
+        target_schema : "platinum_buymed_vn__stg", 
+        // customAssertionsSchema : `dataform_playground_assertions`,
+        // source_schema = source.silver,
+        // description = ``,
+
+        has_lastUpdatedTime : false,
+        has_createdTime : true,
+        bigquery: {
+            partitionBy: "created_date",
+            clusterBy: ["account_id"]
+        }
+    }
+);
+model.createIncremental(
+    ["platinum_0h00","platform_iam_prd_core"],
+    {
+        has_src_created_date : false,
+        ingestCutOffInterval : "INTERVAL 1 MONTH"
+    }
+);
+// model.assertUnique(
+//     ["platinum_09h00", "core_prd_activity"],
+//     {
+//         intervalCheckpoint : `INTERVAL 1 YEAR`,
+//         maxRetry: 2
+//     }
+// )
+// model.createMismatchAssertionView(
+//     ["core_prd_activity"],
+//     {
+//         intervalCheckpoint: `INTERVAL 2 DAY`,
+//         maxRetry: 2,
+//         dependencies : [{"schema":"platinum_buymed_vn","name":"platform_core_prd_activity_activity"}]
+//     }
+// )
+// model.assertMismatch(
+//     ["platinum_09h00", "core_prd_activity"],
+//     {
+//         intervalCheckpoint: `INTERVAL 2 DAY`,
+//         maxRetry: 2,
+        
+//     }
+// )
