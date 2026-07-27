745e0a64c678f51d9eac91a95db04f3c734e17a0
Nguyen Tran <nguyen.tran@buymed.com>
2026-07-27 06:24:17 +0000

    [Platinum] Modify  circa_chat_users: disable assertion

--- definitions/models/staging/platinum_ver2/circa-chat/circa_chat_users.js
+++ definitions/models/staging/platinum_ver2/circa-chat/circa_chat_users.js
@@ -18,27 +18,27 @@ model.createIncremental(
     }
 );
 
-model.assertUnique(
-    ["platinum_0h00", "platinum_12h00", "platinum_unique_assertion", "circa_chat"], {
-        intervalCheckpoint: `INTERVAL 1 YEAR`,
-        maxRetry: 2
-    }
-);
+// model.assertUnique(
+//     ["platinum_0h00", "platinum_12h00", "platinum_unique_assertion", "circa_chat"], {
+//         intervalCheckpoint: `INTERVAL 1 YEAR`,
+//         maxRetry: 2
+//     }
+// );
 
-model.createMismatchAssertionView(
-    ["platinum_staging_assertion_view", "circa_chat"], {
-        intervalCheckpoint: `INTERVAL 2 DAY`,
-        maxRetry: 2,
-        dependencies: [{
-            schema: "platinum_buymed_vn",
-            name: "circa_chat_users"
-        }]
-    }
-);
+// model.createMismatchAssertionView(
+//     ["platinum_staging_assertion_view", "circa_chat"], {
+//         intervalCheckpoint: `INTERVAL 2 DAY`,
+//         maxRetry: 2,
+//         dependencies: [{
+//             schema: "platinum_buymed_vn",
+//             name: "circa_chat_users"
+//         }]
+//     }
+// );
 
-model.assertMismatch(
-    ["platinum_0h00", "platinum_12h00", "platinum_staging_assertion", "circa_chat"], {
-        intervalCheckpoint: `INTERVAL 2 DAY`,
-        maxRetry: 2
-    }
-);
+// model.assertMismatch(
+//     ["platinum_0h00", "platinum_12h00", "platinum_staging_assertion", "circa_chat"], {
+//         intervalCheckpoint: `INTERVAL 2 DAY`,
+//         maxRetry: 2
+//     }
+// );
